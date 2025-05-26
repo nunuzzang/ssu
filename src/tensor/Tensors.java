49b9fb522c90a206f95642a0e475397a907bfde7
nunuzzang <sivwo24@naver.com>
2025-05-26 23:04:09 +0900

    Tensors 확인완료

--- src/tensor/Tensors.java
+++ src/tensor/Tensors.java
@@ -6,7 +6,6 @@ public class Tensors {
     public static Scalar add(Scalar s1, Scalar s2) {
         return ScalarImpl.add(s1, s2);
     }
-
     //25
     public static Scalar multiply(Scalar s1, Scalar s2) {
         return ScalarImpl.multiply(s1, s2);
@@ -17,7 +16,6 @@ public class Tensors {
     public static Vector add(Vector v1, Vector v2) { // 26 [cite: 28]
         return VectorImpl.add(v1, v2);
     }
-
     //27
     public static Vector multiply(Vector v, Scalar s) { // 27 [cite: 28]
         return VectorImpl.multiply(v, s);
@@ -28,17 +26,14 @@ public class Tensors {
     public static Matrix add(Matrix m1, Matrix m2) {
         return MatrixImpl.add(m1, m2);
     }
-
     //29
     public static Matrix multiply(Matrix m1, Matrix m2) {
         return MatrixImpl.multiply(m1, m2);
     }
-
     //32
     public static Matrix attachHMatrix(Matrix m1, Matrix m2) {
         return MatrixImpl.attachHMatrix(m1, m2);
     }
-
     //33
     public static Matrix attachVMatrix(Matrix m1, Matrix m2) {
         return MatrixImpl.attachVMatrix(m1, m2);
