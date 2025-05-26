2d23aac4211f9000f7a18fa322e91a0ddd9cd541
nunuzzang <sivwo24@naver.com>
2025-05-26 23:03:19 +0900

    Factory 확인완료

--- src/tensor/Factory.java
+++ src/tensor/Factory.java
@@ -3,13 +3,11 @@ package tensor;
 import java.io.IOException;
 
 public class Factory {
-
     // Scalar 생성
     //01
     public static Scalar createScalar(String val) {
         return new ScalarImpl(val);
     }
-
     //02
     public static Scalar createScalar(int i, int j) {
         return new ScalarImpl(i, j);
@@ -20,12 +18,8 @@ public class Factory {
     public static Vector createVector(int n, Scalar val) {
         return new VectorImpl(n, val);
     }
-
     //04
-    public static Vector createVector(int i, int j, int n) {
-        return new VectorImpl(i, j, n);
-    }
-
+    public static Vector createVector(int i, int j, int n) { return new VectorImpl(i, j, n); }
     //05
     public static Vector createVector(Scalar[] arr) {
         return new VectorImpl(arr);
@@ -36,26 +30,18 @@ public class Factory {
     public static Matrix createMatrix(int m, int n, Scalar val) {
         return new MatrixImpl(m, n, val);
     }
-
     //07
     public static Matrix createMatrix(int i, int j, int m, int n) {
         return new MatrixImpl(i, j, m, n);
     }
-
     //08
-    public static Matrix createMatrix(String csvPath) throws IOException {
-        return new MatrixImpl(csvPath);
-    }
-
+    public static Matrix createMatrix(String csvPath) throws IOException { return new MatrixImpl(csvPath); }
     //09
     public static Matrix createMatrix(Scalar[][] arr) {
         return new MatrixImpl(arr);
     }
-
     //10
     public static Matrix createIdentityMatrix(int size) {
         return new MatrixImpl(size);
     }
-
-
 }
